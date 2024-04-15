6ca9ef8b867071a6b3b7371f17e78eefadb3499c
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-04-15 21:36:56 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,50 @@
+// //conditional Statement
+//   // if else statement
+//   class test {
+
+
+//     public static void main(String[] args) {
+//         int age = 14;
+//         if (age<18) {
+
+
+//             System.out.println("Not adult");
+//         }
+//         else{
+
+
+//             System.out.println("adult");
+//         }
+        
+//     }
+// }
+
+
+
+
+
+
+// //if else if statemetent
+
+// class test {
+
+//     public static void main(String[] args) {
+
+//         int number =30;
+
+//         if (number>0) {
+
+//             System.out.println("number is positive");
+//         }
+//         else if (number < 0) {
+//             System.out.println("Number is negative");
+
+
+//         }
+//         else{
+
+//             System.out.println("number is Zero");
+//         }
+        
+//     }
+// }
