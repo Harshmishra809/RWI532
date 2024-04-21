34a8a4b925e96d4a8ed7eafe145e9b71ee5c8f52
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-04-21 16:53:57 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,90 @@
+// simple inheritance
+
+// class  Animal {
+
+
+//     void eat () 
+//     {
+//         System.out.println("  eating");
+//     }
+// }
+
+//    public class  Dog extends Animal {
+
+//         public static void main(String[] args) {
+
+//             Dog d = new Dog();
+//              d.eat(); 
+            
+//         }
+//     }
+
+
+// Single inheritance
+
+// class  A{
+//     void showA ()
+//     {
+//         System.out.println("a class method");
+
+
+//     }
+// }
+//  public class B extends A {
+
+//     void showB()
+//     {
+//         System.out.println("B class method");
+        
+
+//     }
+//     public static void main(String[] args) {
+//         A ob1 = new A();
+//         ob1.showA();
+//         B ob2 = new B();
+//         ob2.showA();
+//         ob2.showB();
+//     }
+// }
+
+// multilevel inheritance 
+
+
+// class A {
+
+//     void showA ()
+//     {
+
+//         System.out.println("a class method");
+//     }
+// }
+
+//  class B extends  A {
+
+//     void showB ()
+//     {
+//         System.out.println("B class method");
+//     }
+// }
+//  public class C extends B {
+
+
+//     void showC()
+//     {
+
+//         System.out.println ("C Class method");
+//     }
+//     public static void main(String[] args) {
+//         C ob3 = new C();
+//         ob3.showA();
+//         ob3.showB();
+//         ob3.showC();
+//     }
+// }
+
+
+
+
+
+
+
