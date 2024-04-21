8bccb186ca8a3dcf680fd1c02615f2cd8dce550e
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-04-21 11:51:45 +0530

    Add files via upload

---
+++ Strbuffe.java
@@ -0,0 +1,148 @@
+// StringBuffer  in java
+
+
+  
+//   public class   Strbuffe{
+
+//     public static void main(String[] args) {
+
+//         StringBuffer sb = new StringBuffer(" Harsh");
+//         System.out.println(sb);
+        
+//     }
+//  }
+
+
+
+
+// set char at index  function in java 
+
+// public class Strbuffe {
+
+//     public static void main(String[] args) {
+  
+//       StringBuffer sb = new StringBuffer( "Harsh");
+  
+  
+//     sb.setCharAt(0, 'p');
+//     System.out.println(sb);
+      
+//     }
+//   }
+
+
+// insert  method in java 
+
+// public class Strbuffe {
+
+//       public static void main(String[] args) {
+    
+//         StringBuffer sb = new StringBuffer( "Harsh");
+//     sb.insert(2, 's');
+//     System.out.println(sb);
+    
+    
+        
+//       }
+//     }
+  
+  
+
+// delete   method in java 
+
+
+// public class Strbuffe {
+
+//         public static void main(String[] args) {
+      
+//           StringBuffer sb = new StringBuffer( "Harsh");
+//       sb.delete(2, 3);
+//       System.out.println(sb);
+          
+//         }
+//       }
+
+    
+
+// append   method in java 
+
+
+
+// public class Strbuffe {
+
+//           public static void main(String[] args) {
+        
+//             StringBuffer sb = new StringBuffer( "Harsh");
+//         sb.append("mishra");
+//         System.out.println(sb);
+            
+//           }
+//         }
+
+
+// length  method in java 
+
+
+
+// public class Strbuffe {
+
+//           public static void main(String[] args) {
+        
+//             StringBuffer sb = new StringBuffer( "Harsh");
+    
+//         System.out.println(sb.length());
+            
+//         }
+//       }
+
+
+// sub String method in java
+
+
+// public class Strbuffe {
+
+//             public static void main(String[] args) {
+          
+//               StringBuffer sb = new StringBuffer( "Harsh");
+      
+//           System.out.println (sb.substring(3));
+              
+//           }
+//         }
+
+
+// reverse method  
+
+
+// public class Strbuffe {
+
+//               public static void main(String[] args) {
+            
+//                 StringBuffer sb = new StringBuffer( "Harsh");
+        
+//             System.out.println (sb.reverse());
+                
+//             }
+//           }
+
+
+
+
+// subSequence method in java 
+
+
+// public class Strbuffe {
+
+//               public static void main(String[] args) {
+            
+//                 StringBuffer sb = new StringBuffer( "HarshMishra");
+        
+//             System.out.println (sb.subSequence(3, 6));
+                
+//             }
+//           }
+
+
+
+
+
