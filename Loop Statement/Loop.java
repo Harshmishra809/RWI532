c7850cd710c9a03a0c842310cbe3e93ed0a4cb8f
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-05 06:50:54 +0530

    Add files via upload

---
+++ Loop Statement/Loop.java
@@ -0,0 +1,68 @@
+ // for loop
+
+ //example 1
+ 
+ 
+//  public class test {
+
+//     public static void main(String[] args) {
+        
+//         for (int counter =0;counter<11; counter++)
+
+//         System.out.println(counter);
+//     }
+// }
+
+//example 2 
+
+
+// public class test {
+
+//     public static void main (String[] args) {
+
+//         for (int i =10; i>0;i-- )
+
+//         System.out.println(i);
+//     }
+// }
+
+
+
+// while loop 
+//example 1
+
+// class test  {
+
+
+//     public static void main(String[] args) {
+//         int i =0;
+//         while(i<11) {
+//             System.out.println(i);
+//             i=i+1;
+
+
+            
+//         }
+        
+        
+//     }
+// }
+
+//example2
+
+//  public class test {
+
+//     public static void main(String[] args) {
+        
+//         int i=10;
+//         while(i<20) {
+
+
+//             System.out.println(i);
+//             i=i+1;
+//         }
+//     }
+// }
+
+
+
