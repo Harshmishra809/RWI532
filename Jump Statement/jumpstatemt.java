ea2ac5585f31763fa594706e7a5ebd5567c8bcb1
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-05 07:33:56 +0530

    Add files via upload

---
+++ Jump Statement/jumpstatemt.java
@@ -0,0 +1,76 @@
+
+
+// example 1
+
+//  public class test {
+
+//     public static void main(String[] args) {
+//         for (int i =0;i<10;i++)
+//       {
+
+//         if (i==5)
+//         {
+//             break;
+//         }
+//         System.out.println(i);
+       
+//       }
+      
+//     }
+// }
+
+// example 2 
+// class test {
+
+//   public static void main(String[] args) {
+//     for (int i=0;i<20;i++)
+
+//     {
+
+//       if (i==15)
+//       {
+
+//         break;
+//       }
+//       System.out.println(i);
+//     }
+//   }
+// }
+
+
+//continue statement in java 
+//example 1
+//  public class test {
+
+//   public static void main(String[] args) {
+//     for (int i =0; i<30;i++)
+//     {
+
+//       if (i==25)
+    
+//     {
+//          continue;
+//     }
+//     System.out.println(i);
+
+//   }
+//   }
+// }
+
+//example 2
+// public class test {
+
+//   public static void main(String[] args) {
+//     for (int i =20;i<50;i++) 
+//     {
+//       if (i==45)
+    
+//     {
+//       continue;
+//     }
+//     System.out.println(i);
+
+//   }
+    
+//   }
+// }
