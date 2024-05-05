e7b02c063a306b47c1e456ce2eed0f05f9536ced
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-05 07:10:25 +0530

    Add files via upload

---
+++ String/String.java
@@ -0,0 +1,113 @@
+//  public class Str {
+
+//     public static void main(String[] args) {
+
+//          String firstname = "John";
+//          String Lastname = "mark";
+//          String fullname =  firstname+ " " +Lastname;
+
+//          System.out.println(fullname);
+        
+//     }
+// }
+
+
+//length method in string 
+
+// public class Str {
+//     public static void main(String[] args) {
+
+//         String  Name = " HarshMishra";
+//         System.out.println(Name.length());
+        
+//     }
+
+    
+// }
+
+
+
+// CharAt method in java 
+
+// public class Str {
+//     public static void main(String[] args) {
+
+//          String name = "HarshMishra";
+//          for (int i =0;i<name.length();i++) {
+
+//             System.out.println(name.charAt(i));
+//          }
+
+        
+//     }
+
+
+// }
+
+
+
+// compare two string
+
+
+// public class  Str {
+
+//     public static void main(String[] args) {
+//         String firstname =  "Harsh";
+//         String lastname = "Harsh";
+
+//       if (firstname.compareTo(lastname)==0) {
+//         System.out.println("String are equals"); 
+
+
+
+//       }
+//       else {
+//         System.out.println("not equals");
+//       }
+        
+//     }
+// }
+
+
+
+
+// double Equal (==) operator
+
+// public class Str {
+//     public static void main(String[] args) {
+
+//       String  firstname = "Harsh";
+//       String   Lastname = "mishra";
+
+//       if (firstname==Lastname) {
+//         System.out.println("String are equlas");
+
+
+//       }
+//       else {
+
+//         System.out.println("String are not equlqas");
+//       }
+
+        
+//     }
+
+    
+// }
+
+// SubString  method
+
+
+// class Str{
+
+//     public static void main(String[] args) {
+
+//         String  fullname = "Harsh  Mishra";
+
+//         String name =  fullname.substring(7, fullname.length());
+//         System.out.println(name);
+        
+//     }
+// }
+
+
