4c51bb98ed7e0540bd5a54b9cf6e82a699b57466
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-05 07:24:28 +0530

    Add files via upload

---
+++ oops  in java/dataabstraction.java
@@ -0,0 +1,33 @@
+
+
+abstract class Shape {
+    public abstract void draw();
+}
+
+class Rectangle extends Shape {
+    
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+
+class Circle extends Shape {
+    
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+
+public class dataabstraction {
+    public static void main(String[] args) {
+        
+        Shape shape1 = new Rectangle();
+        Shape shape2 = new Circle();
+
+        
+        shape1.draw();
+        shape2.draw();
+    }
+}
