f00134eb43ef8aca5f4d8b599cc1fc0f63e6690d
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-07 02:50:47 +0530

    Add files via upload

---
+++ Collection framework/Linklist.java
@@ -0,0 +1,35 @@
+import java .util.LinkedList;
+
+public class Linklist {
+
+    public static void main(String[] args) {
+        LinkedList<String> name = new LinkedList<String>  ();
+        name.add ("Harsh");   // add method
+        name.add("sahil");
+        name.add("jatin");
+        name.add("Lucky");
+        System.err.println(name);
+
+        name.addFirst("Rohit");
+        name.addLast("vicky");
+        System.out.println(name);
+        name.add(3, "Rahul");
+        System.out.println(name);
+
+        name.remove(3);  // remove method
+        System.out.println(name);
+        name.removeFirst();
+        System.out.println(name);
+        name.removeLast();
+        System.out.println(name); 
+
+                                  // for each loop
+           for(String str:name)
+           {
+
+            System.out.println(str);
+           }
+        
+    }
+    
+}
