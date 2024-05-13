ad1fe95186ec213797af35bbe012d34c2f00885c
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-13 17:47:16 +0530

    Add files via upload

---
+++ Java_8/FunctionInterface/FunctionalInterfaceExample2.java
@@ -0,0 +1,19 @@
+interface Doable{  
+    default void doIt(){  
+        System.out.println("Do it now");  
+    }  
+}  
+@FunctionalInterface  
+interface Sayable extends Doable{  
+    void say(String msg);   // abstract method  
+}  
+public class FunctionalInterfaceExample2 implements Sayable{  
+    public void say(String msg){  
+        System.out.println(msg);  
+    }  
+    public static void main(String[] args) {  
+        FunctionalInterfaceExample2 fie = new FunctionalInterfaceExample2();  
+        fie.say("Hello there");  
+        fie.doIt();  
+    }  
+}  
