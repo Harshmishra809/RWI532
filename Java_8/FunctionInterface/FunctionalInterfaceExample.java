315dc32362885fb5bcde48652bcd322a7092c1b8
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-13 17:41:47 +0530

    Add files via upload

---
+++ Java_8/FunctionInterface/FunctionalInterfaceExample.java
@@ -0,0 +1,12 @@
+interface sayable{  
+    void say(String msg);  
+}  
+public class FunctionalInterfaceExample implements sayable{  
+    public void say(String msg){  
+        System.out.println(msg);  
+    }  
+    public static void main(String[] args) {  
+        FunctionalInterfaceExample fie = new FunctionalInterfaceExample();  
+        fie.say("Hello");  
+    }  
+}  
