fca1ed421ac128a81befc1d55b7918a4ca96486c
Harshmishra809 <129086713+Harshmishra809@users.noreply.github.com>
2024-05-29 16:42:42 +0530

    Add files via upload

---
+++ Hibernate/One to Many/Employee.java
@@ -0,0 +1,42 @@
+package Onetomany;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department; 
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
